a45c4d1963e5fd2ac85a9dd4e093f70beafeb99e
hazed <aleksey.makaseev@gmail.com>
2021-12-03 14:48:30 +0200

    add base ui

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+      // gameObject.SetEnable(false);
+    }
+
+}
